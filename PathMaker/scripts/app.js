14923f97ccc2168203f3085f0cae5a017e6cd4fa
Boris Penev <borispenev89@gmail.com>
2013-10-11 15:12:55 -0500

    Changes made by Boris Penev (borispenev89@gmail.com).

--- PathMaker/scripts/app.js
+++ PathMaker/scripts/app.js
@@ -4,6 +4,8 @@
 
     document.addEventListener("deviceready", function () {
         app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
+        
+        
     }, false);
 
     app.changeSkin = function (e) {
